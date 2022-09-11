cb7e64a8c51c188e34862e331ab65c3c0aa50505
Percy Bolmér <percybolmer@metabytes.se>
2022-09-11 07:38:04 +0200

    fixed heartbeats

--- client.go
+++ client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -18,15 +19,24 @@ type Client struct {
 	// manager is the manager used to manage the client
 	manager *Manager
 	// egress is used to avoid concurrent writes on the WebSocket
-	egress chan []Event
+	egress chan Event
 }
+
+var (
+	// pongWait is how long we will await a pong response from client
+	pongWait = 10 * time.Second
+	// pingInterval has to be less than pongWait, We cant multiply by 0.9 to get 90% of time
+	// Because that can make decimals, so instead *9 / 10 to get 90%
+	// The reason why it has to be less than PingRequency is becuase otherwise it will send a new Ping before getting response
+	pingInterval = (pongWait * 9) / 10
+)
 
 // NewClient is used to initialize a new Client with all required values initialized
 func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 	return &Client{
 		connection: conn,
 		manager:    manager,
-		egress:     make(chan []Event),
+		egress:     make(chan Event),
 	}
 }
 
@@ -39,6 +49,16 @@ func (c *Client) readMessages() {
 		// function is done
 		c.manager.removeClient(c)
 	}()
+
+	// Configure Wait time for Pong response, use Current time + pongWait
+	// This has to be done here to set the first initial timer.
+	if err := c.connection.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
+		log.Println(err)
+		return
+	}
+	// Configure how to handle Pong responses
+	c.connection.SetPongHandler(c.pongHandler)
+
 	// Loop Forever
 	for {
 		// ReadMessage is used to read the next message in queue
@@ -66,9 +86,19 @@ func (c *Client) readMessages() {
 	}
 }
 
+// pongHandler is used to handle PongMessages for the Client
+func (c *Client) pongHandler(pongMsg string) error {
+	// Current time + Pong Wait time
+	log.Println("pong")
+	return c.connection.SetReadDeadline(time.Now().Add(pongWait))
+}
+
 // writeMessages is a process that listens for new messages to output to the Client
 func (c *Client) writeMessages() {
+	// Create a ticker that triggers a ping at given interval
+	ticker := time.NewTicker(pingInterval)
 	defer func() {
+		ticker.Stop()
 		// Graceful close if this triggers a closing
 		c.manager.removeClient(c)
 	}()
@@ -86,6 +116,7 @@ func (c *Client) writeMessages() {
 				// Return to close the goroutine
 				return
 			}
+
 			data, err := json.Marshal(message)
 			if err != nil {
 				log.Println(err)
@@ -96,6 +127,13 @@ func (c *Client) writeMessages() {
 				log.Println(err)
 			}
 			log.Println("sent message")
+		case <-ticker.C:
+			log.Println("ping")
+			// Send the Ping
+			if err := c.connection.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				log.Println("writemsg: ", err)
+				return // return to break this goroutine triggeing cleanup
+			}
 		}
 
 	}
